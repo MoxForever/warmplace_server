9b4213e911baebd1b012ab8c6be968697c7ba317
MoxForever <moxforever10@gmail.com>
2026-04-12 11:18:26 +0600

    fix fish

--- modules/virtualization/docker-update.fish
+++ modules/virtualization/docker-update.fish
@@ -1,6 +1,6 @@
 complete -c docker-update -f -d "Update and restart a Docker deployment"
-complete -c docker-update -n "__fish_use_subcommand_from_list" -a "(__fish_docker_update_apps)" -d "App name"
+complete -c docker-update -a "(__fish_docker_update_apps)" -d "App name"
 
 function __fish_docker_update_apps --description "List available docker-deploy app names"
-    systemctl list-unit-files --no-pager 2>/dev/null | grep -oP '(?<=docker-deploy-)\w+(?=\.service)' | sort -u
+    systemctl list-unit-files --no-pager 2>/dev/null | grep 'docker-deploy-' | awk -F'docker-deploy-' '{print $2}' | awk -F'.service' '{print $1}' | sort -u
 end
